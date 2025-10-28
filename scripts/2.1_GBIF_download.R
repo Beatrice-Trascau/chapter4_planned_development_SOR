e46acc3ca897d13349bbb8ec69c768f6a9cbd452
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-10-28 13:26:34 +0100

    feat: script to download GBIF occurrences

---
+++ scripts/2.1_GBIF_download.R
@@ -0,0 +1,39 @@
+##----------------------------------------------------------------------------##
+# PAPER 4: PLANNED DEVELOPMENT AREA AND SPECIES OCCURRENCE RECORDS
+# 2.1_GBIF_download
+# This script contains code which downloads the GBIF Occurrence records used in
+# the analysis
+##----------------------------------------------------------------------------##
+
+# 1. CONNECT TO GBIF -----------------------------------------------------------
+
+# Setting username, password and email 
+Sys.setenv(GBIF_USER = "beatrice_trasc.au") # change with your details
+Sys.setenv(GBIF_PWD = "I97zB%$z")
+Sys.setenv(GBIF_EMAIL = "beatrice.m.trascau@ntnu.no")
+
+# 2. CREATE DOWNLOAD REQUEST ---------------------------------------------------
+
+# Send download request
+download_key <- occ_download(pred("gadm", "NOR"), # Norway
+                             pred_gte("year", 2008), # greater than or equal to year 2008
+                             pred_lte("year", 2024), # lower than or equal to 2024
+                             pred("hasCoordinate", TRUE), 
+                             format = "DWCA") # download as a Darwin Core Archive file
+
+# Check progress
+occ_download_wait(download_key)
+
+# Download key: 
+# Download link: 
+# DOI: 
+
+# 3. IMPORT GBIF DOWNLOAD AND SAVE ---------------------------------------------
+
+# Import data
+occurrence <- occ_download_get(download_key) |>
+  occ_download_import()
+
+# Save to file
+save(occurrence, file = here::here("data","raw_data",
+                                   "occurrences_chapter4_October2025.txt"))
