1c1100d4fc8f66c323e27983e24985ab7ff50923
Beatrice Maria Trascau <beatrimt@gosling.win.ntnu.no>
2026-08-07 12:18:44 +0000

    feat: new script to test H4ab

---
+++ scripts/6.1_H4ab_models.R
@@ -0,0 +1,7 @@
+##----------------------------------------------------------------------------##
+# PAPER 4: PLANNED DEVELOPMENT AREA AND SPECIES OCCURRENCE RECORDS
+# 6.1_H4ab_models
+# This script contains code to test H4a: Area plan polygons have a
+# greater number of red-listed SOR than areas not planned for development, and
+# H4b: the share of red-listed SOR inside polygons increases with area.
+##----------------------------------------------------------------------------##
