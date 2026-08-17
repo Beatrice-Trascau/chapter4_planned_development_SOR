984bb900d29b463863df783aa0ee1e94505893c3
Beatrice Maria Trascau <beatrimt@gosling.win.ntnu.no>
2026-08-17 11:44:21 +0000

    feat: new script to test H5ab

---
+++ scripts/7.1_H5ab_models.R
@@ -0,0 +1,7 @@
+##----------------------------------------------------------------------------##
+# PAPER 4: PLANNED DEVELOPMENT AREA AND SPECIES OCCURRENCE RECORDS
+# 7.1_H5ab_models
+# This script contains code to test Hypothesis 5a: Development Polygons have a
+# greatr number of alien SOR than areas not planned for development and
+# H5b: the share of alien SOR inside polygons increases with area
+##----------------------------------------------------------------------------##
