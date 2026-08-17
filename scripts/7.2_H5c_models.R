aea3e5f4ebc6813142187ff70c624237e78a4560
Beatrice Maria Trascau <beatrimt@gosling.win.ntnu.no>
2026-08-17 14:57:42 +0000

    feat: set up models

--- scripts/7.2_H5c_models.R
+++ scripts/7.2_H5c_models.R
@@ -50,20 +50,106 @@ print(tapply(richness_data$n_species, richness_data$polygon_type, mean))
 # Buffer     Development 
 # 0.12697007  0.07493013 
 
+# 3. FIT MODELS (ALL ALIENS) ---------------------------------------------------
 
+## 3.1. H5c Negative binomial with full interaction ----------------------------
 
+# Set up model
+h5c_nbinom_full <- glmmTMB(n_species ~ polygon_type * (log_area_c + land_cover_name) +
+                             (1 | kommune_factor/pair_id_factor),
+                           data = richness_data,
+                           family = nbinom2)
 
+# Save model output
+save(h5c_nbinom_full,
+     file = here::here("data", "models", "h5c_nbinom_full.RData"))
 
+## 3.2. H5c Negative binomial, additive ----------------------------------------
 
+# Set up model
+h5c_nbinom_additive <- glmmTMB(n_species ~ polygon_type + log_area_c +
+                                 land_cover_name +
+                                 (1 | kommune_factor/pair_id_factor),
+                               data = richness_data,
+                               family = nbinom2)
 
+# Save model output
+save(h5c_nbinom_additive,
+     file = here::here("data", "models", "h5c_nbinom_additive.RData"))
 
+# Compare models
+AICtab(h5c_nbinom_full, h5c_nbinom_additive, base = TRUE)
 
+## 3.3. H5c Zero-inflated, interaction -----------------------------------------
 
+# Set up model
+h5c_zinb_full <- glmmTMB(n_species ~ polygon_type * (log_area_c + land_cover_name) +
+                           (1 | kommune_factor/pair_id_factor),
+                         data = richness_data,
+                         family = nbinom2,
+                         ziformula = ~ polygon_type + log_area_c)
 
+# Save model
+save(h5c_zinb_full, file = here::here("data", "models", "h5c_zinb_full.RData"))
 
+## 3.4. H5c Zero-inflated, additive --------------------------------------------
 
+# Set up model
+h5c_zinb_additive <- glmmTMB(n_species ~ polygon_type + log_area_c + land_cover_name +
+                               (1 | kommune_factor/pair_id_factor),
+                             data = richness_data,
+                             family = nbinom2,
+                             ziformula = ~ polygon_type + log_area_c)
 
+# Save model output
+save(h5c_zinb_additive, file = here::here("data", "models", "h5c_zinb_additive.RData"))
 
+# Compare the models
+AICtab(h5c_zinb_full, h5c_zinb_additive, base = TRUE)
 
+# Use the model with the lower AIC
+best_model_h5c <- h5c_zinb_full
 
+## 3.5. H5c Hurdle (zero-truncated NB), interaction ----------------------------
 
+# Set up model
+h5c_hurdle_full <- glmmTMB(n_species ~ polygon_type * (log_area_c + land_cover_name) +
+                             (1 | kommune_factor/pair_id_factor),
+                           data = richness_data,
+                           family = truncated_nbinom2,
+                           ziformula = ~ polygon_type + log_area_c)
+
+# Save model output
+save(h5c_hurdle_full, file = here::here("data", "models", "h5c_hurdle_full.RData"))
+
+## 3.6. H5c Hurdle (zero-truncated NB), additive -------------------------------
+
+# Set up model
+h5c_hurdle_additive <- glmmTMB(n_species ~ polygon_type + log_area_c + land_cover_name +
+                                 (1 | kommune_factor/pair_id_factor),
+                               data      = richness_data,
+                               family    = truncated_nbinom2,
+                               ziformula = ~ polygon_type + log_area_c)
+
+# Save modek output
+save(h5c_hurdle_additive,
+     file = here::here("data", "models", "h5c_hurdle_additive.RData"))
+
+# Compare models
+AICtab(h5c_hurdle_full, h5c_hurdle_additive, base = TRUE)
+
+# Check convergence of the hurdle models
+if (h5c_hurdle_full$sdr$pdHess) {
+  cat("\nH5c hurdle (full) converged successfully\n")
+} else {
+  cat("\nWarning: H5c hurdle (full) may not have converged properly\n")
+}
+
+## 3.7. Compare all three model families ---------------------------------------
+
+# Extract AIC
+cat("\nNegative binomial vs zero-inflated vs hurdle (full versions):\n")
+AICtab(h5c_nbinom_full, h5c_zinb_full, h5c_hurdle_full, base = TRUE)
+
+# Select best H5c model
+best_model_h5c <- h5c_zinb_full
